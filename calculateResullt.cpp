f475191b734179e73ab8a44165145eee30607a33
Arek <bajurski@hotmail.comgit config --global user.email bajurski@hotmail.comgit config --global user.email bajurski@hotmail.com>
2022-05-27 01:38:36 +0200

    Stubs of test functions were prototyped

---
+++ calculateResullt.cpp
@@ -0,0 +1,27 @@
+//
+// Created by arek on 27.05.22.
+//
+
+#include "calculateResullt.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void test_1()
+{
+    string s1 = "test_1_1.dat";
+    string s2 = "test_1_2.dat";
+
+}
+void test_2()
+{
+    string s3 = "test_2_1.dat";
+    string s4 = "test_2_2.dat";
+}
+void test_3()
+{
+    string s5 = "test_3_1.dat";
+    string s6 = "test_3_2.dat";
+}
---
+++ calculateResullt.h
@@ -0,0 +1,14 @@
+//
+// Created by arek on 27.05.22.
+//
+
+#ifndef BER_KALKULATOR_CALCULATERESULLT_H
+#define BER_KALKULATOR_CALCULATERESULLT_H
+
+#include <string>
+
+void test_1();
+void test_2();
+void test_3();
+
+#endif //BER_KALKULATOR_CALCULATERESULLT_H
