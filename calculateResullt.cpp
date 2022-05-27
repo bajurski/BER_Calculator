76bbf717d077c52d3a25f1d01edc536a1eee6a5a
bajurski <bajurski@hotmail.com>
2022-05-27 22:08:41 +0200

    Intermediate from Mac.

--- calculateResullt.cpp
+++ calculateResullt.cpp
@@ -1,7 +1,7 @@
 //
 // Created by arek on 27.05.22.
 //
-
+#include <iostream>
 #include "calculateResullt.h"
 #include <fstream>
 #include <iostream>
@@ -9,34 +9,55 @@
 
 using namespace std;
 
-testData loadDataFromFile(string  &path)
-{
-    char num1, num2;
-    fstream file1(path , ios::binary | ios::out );
-    if(!file1.is_open()) {
-        cout<<"Error in opening file 1 for test 1"<<endl;
+testData loadDataFromFile(string  path1, string  path2) {
+    char num;
+    string path{};
+    bool switcher{true};
+    int counter{0};
+    testData testdat;
+
+    while (switcher) {
+    if (counter==0) path = path1;
+    if (counter==1) path = path2;
+    fstream file(path, ios::binary | ios::out);
+    if (!file.is_open()) {
+        cout << "Error in opening file." << endl;
     } else {
         for (int i = 0; i < 100; ++i) {
-            file1.read((char *) &num1, sizeof(num1));
+           if (path == path1) {
+               file.read((char *) &num, sizeof(num));
+               testdat.data1.push_back(num);
+           } else if(path == path2) {
+               file.read((char *) &num, sizeof(num));
+               testdat.data2.push_back(num);
 
+           }
         }
-       // cout<<"Populating the file 1 for test 1 was successful ."<<endl;
-        file1.close();
+        // cout<<"Populating the file 1 for test 1 was successful ."<<endl;
+        file.close();
     }
+    counter++;
+    if(counter == 1) switcher = false;
+}
+        return testdat;
 }
 void test_1()
 {
+    testData test_1_data;
     string s1 = "test_1_1.dat";
     string s2 = "test_1_2.dat";
-
+    test_1_data = loadDataFromFile(s1,s2);
+    for (int i = 0; i < test_1_data.data1.size(); ++i) {
+        cout << (char*) &test_1_data.data1[i]<<endl;
+    }
 }
 void test_2()
 {
-    string s3 = "test_2_1.dat";
-    string s4 = "test_2_2.dat";
+    const string s3 = "test_2_1.dat";
+    const string s4 = "test_2_2.dat";
 }
 void test_3()
 {
-    string s5 = "test_3_1.dat";
-    string s6 = "test_3_2.dat";
+    const string s5 = "test_3_1.dat";
+    const string s6 = "test_3_2.dat";
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "dataFiles.h"
-
+#include "calculateResullt.h"
 int main() {
     createFilesForTest_1();
     createFilesForTest_2();
     createFilesForTest_3();
 
+    test_1();
 
     return 0;
 }
