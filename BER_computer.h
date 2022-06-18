51e5888d15bad7e6adc24e7150b428551a15986c
bajurski <bajurski@hotmail.com>
2022-06-18 22:29:15 +0200

    Final commit

--- BER_computer.h
+++ BER_computer.h
@@ -4,6 +4,9 @@
 
 #ifndef BER_KALKULATOR_BER_COMPUTER_H
 #define BER_KALKULATOR_BER_COMPUTER_H
+#include<time.h>
+
+
 
 int calcWrongBits(char ch1, char ch2);
 
--- calculateResullt.cpp
+++ calculateResullt.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 std::stringstream msg;
+clock_t calcTime1;
+clock_t calcTime2;
 
 std::string timeConverter(int sec) {
     string convertedTime{};
@@ -54,7 +56,6 @@ void test_1()
 {
     openLog("tests.txt");
 
-
     testData test_1_data;
     const string s1 = "test_1_1.dat";
     const string s2 = "test_1_2.dat";
@@ -65,6 +66,7 @@ void test_1()
     clock_t time = clock();
     test_1_data = loadDataFromFile(s1,s2);
     size_t totalNumOfBits = test_1_data.data1.size()*2*8;
+    calcTime1=clock();
     for (int i = 0; i <  test_1_data.data1.size(); ++i) {
         val_1 = test_1_data.data1.at(i);
         val_2 = test_1_data.data2[i];
@@ -72,9 +74,11 @@ void test_1()
         BER_value = badBits/(double)totalNumOfBits;
 
     }
+    calcTime2=clock();
     msg<<"Test 1 performed"<<endl;
     msg<<"Number of bad bits in test 1 files : "<< badBits<<endl;
     msg<<"Bit Error Rate for files in Test 1 : "<< BER_value<<endl;
+    msg<<"Time of the calculation was: " <<fixed<<setprecision(10)<< ((float)calcTime2 - calcTime1) / CLOCKS_PER_SEC << " sec " << std::endl;
     msg<<"================================================"<<endl;
     saveLog(msg.str());
     closeLog();
@@ -92,17 +96,19 @@ void test_2()
     double BER_value;
     test_2_data = loadDataFromFile(s3,s4);
     size_t totalNumOfBits = test_2_data.data1.size() * 2* 8;
-    clock_t time = clock();
+    calcTime1=clock();
     for (int i = 0; i < 100; ++i) {
         val_1 = test_2_data.data1[i];
         val_2 = test_2_data.data2[i];
         badBits+=calcWrongBits(reinterpret_cast<char>(val_1), reinterpret_cast<char>(val_2));
         BER_value = badBits/(double)totalNumOfBits;
     }
+    calcTime2=clock();
     msg<<"Test 2 performed"<<endl;
     msg<<"Number of bad bits in test 2 files : "<< badBits<<endl;
     msg<<"Bit Error Rate for files in Test 2 : "<< BER_value<<endl;
     msg<<"End of log for test 2"<<endl;
+    msg<<"Time of the calculation was: " <<fixed<<setprecision(10)<< ((float)calcTime2 - calcTime1) / CLOCKS_PER_SEC << " sec " << std::endl;
     msg<<"================================================"<<endl;
     saveLog(msg.str());
     closeLog();
@@ -119,7 +125,7 @@ void test_3()
     double BER_value;
     test_3_data = loadDataFromFile(s5,s6);
     size_t totalNumOfBits = test_3_data.data2.size()*2*8;
-    clock_t time = clock();
+    calcTime1=clock();
 
     for (int i = 0; i < 400000000; ++i) {
         val_1 = test_3_data.data1[i];
@@ -127,10 +133,12 @@ void test_3()
         badBits+=calcWrongBits(reinterpret_cast<char>(val_1), reinterpret_cast<char>(val_2));
         BER_value = badBits/(double)totalNumOfBits;
     }
+    calcTime2=clock();
     msg<<"Test 3 performed"<<endl;
     msg<<"Number of bad bits in test 3 files : "<< badBits<<endl;
     msg<<"Bit Error Rate for files in Test 3 : "<< BER_value<<endl;
     msg<<"End of log for test 3"<<endl;
+    msg<<"Time of the calculation was: " <<fixed<<setprecision(10)<< ((float)calcTime2 - calcTime1) / CLOCKS_PER_SEC << " sec " << std::endl;
     msg<<"======================================================="<<endl;
     saveLog(msg.str());
     closeLog();
